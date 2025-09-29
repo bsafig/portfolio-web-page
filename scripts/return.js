22478151949728c9e3cbf6fe9e53fafc28577702
bsafig <ben@safigan.com>
2025-09-28 23:50:34 -0600

    add frc page

---
+++ scripts/return.js
@@ -0,0 +1,7 @@
+// Return button functionality
+
+console.log("return.js loaded");
+
+document.getElementById('return-btn').addEventListener('click', function() {
+    window.location.href = '../index.html';
+});
